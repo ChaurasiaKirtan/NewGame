6be97bf346a1d05e4f0c9f6d73fa109086aa0ee0
kchaurasia <kchaurasia@PF1C1A1B.ad.wcpss.net>
2022-01-25 17:58:55 -0500

    Regards Msg added

--- src/Training/Stage.java
+++ src/Training/Stage.java
@@ -5,6 +5,7 @@ public class Stage {
 	public static void main(String[] args) {
 		System.out.println("Hello Welcome to the world of fortnite");
 System.out.println("this is where it ends");
+System.out.println("Regard Aly");
 	}
 
 }
